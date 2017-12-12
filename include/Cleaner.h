e81ff4fc0ad76b572e4f1f4c89be7615a795a465
VBot2410 <vaibhav24.vb@gmail.com>
2017-12-12 05:27:41 -0500

    Task_ID:Dev_03_04, Added header files for classes Goals and Cleaner. Cleaner is a derived class of Goals. Both header files contain all variable and function declarations.

---
+++ include/Cleaner.h
@@ -0,0 +1,66 @@
+/**
+ * @file cleaner.h
+ * @brief This file contains function declarations for Cleaner class. 
+ * 
+ *
+ * @author Vaibhav Bhilare
+ * @copyright 2017, Vaibhav Bhilare
+ *
+ * MIT License
+ * Copyright (c) 2017 Vaibhav Bhilare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* --Includes-- */
+#ifndef CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_CLEANER_H_
+#define CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_CLEANER_H_
+#include "Goals.h"
+#include <vector>
+
+/**
+ * @brief Cleaner class
+ *        This class is a protected derived class of base class Goals.
+ *        This class contains functions which initialize action client.
+ *        This class also uses functions from Goals class to send goals
+ *        sequentially to move_base.
+ */
+class Cleaner:protected Goals {
+ public:  // Public access specifier
+/**
+ * @brief Constructor for Cleaner Class
+ *        Takes Goal Points and stores them in a vetor of vectors.
+ * @param Goal_Points vector of vectors of type double
+ * @return void None
+ */
+explicit Cleaner(std::vector<std::vector<double>>& Goal_Points);
+/**
+ * @brief Clean_Room Initializes action client to communicate with move_base
+ *                   This function sends goals to move_base.
+ * @return void None
+ */
+void Clean_Room();
+/**
+ * @brief Destructor for Cleaner Class
+ * @param None
+ * @return void None
+ */
+virtual ~Cleaner();
+};
+#endif  // CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_CLEANER_H_
---
+++ include/Goals.h
@@ -0,0 +1,99 @@
+/**
+ * @file Goals.h
+ * @brief This file contains function declarations for Goals class. 
+ * 
+ *
+ * @author Vaibhav Bhilare
+ * @copyright 2017, Vaibhav Bhilare
+ *
+ * MIT License
+ * Copyright (c) 2017 Vaibhav Bhilare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/* --Includes-- */
+#ifndef CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_GOALS_H_
+#define CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_GOALS_H_
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <tf/transform_datatypes.h>
+#include <vector>
+
+/**
+ * @brief Goals class
+ *        This class is a base class which contains general use functions.
+ *        This class contains functions which sets goal point's X,Y,orientation.
+ */
+class Goals{
+ public:  // Public Access Specifier
+/**
+ * @brief Constructor for Goals Class
+ * @param None
+ * @return void None
+ */
+Goals();
+
+/**
+ * @brief Set_Current_X a function to set goal's X coordinate.
+ *        This function sets the goal's X coordinate when given a goal point.
+ * @param Next_Goal is a Goal point of type double vector.
+ * @param goal is an instance of a message of type move_base_msgs::MoveBaseGoal
+ * @return double type Goal Point's X coordinate.
+ */
+double Set_Current_X(std::vector<double>& Next_Goal,
+                        move_base_msgs::MoveBaseGoal & goal);
+
+/**
+ * @brief Set_Current_Y a function to set goal's Y coordinate.
+ *        This function sets the goal's Y coordinate when given a goal point.
+ * @param Next_Goal is a Goal point of type double vector.
+ * @param goal is an instance of a message of type move_base_msgs::MoveBaseGoal
+ * @return double type Goal Point's Y coordinate.
+ */
+double Set_Current_Y(std::vector<double>& Next_Goal,
+                        move_base_msgs::MoveBaseGoal & goal);
+
+/** Get_Goal of type double vector, Stores Current Goal Point Data */
+std::vector<double> Get_Goal;
+
+/**
+ * @brief Destructor for Goals Class
+ * @param None
+ * @return void None
+ */
+virtual ~Goals();
+
+ protected:  // Protected Access Specifier
+/** Goal_Points of type vector of double vectors, Stores all Goal Points */
+std::vector<std::vector<double>> Goal_Points;
+
+/**
+ * @brief Set_Current_Orientation a function to set goal's Orientation.
+ *        This function sets the goal's orientation when given a goal point.
+ * @param Next_Goal is a Goal point of type double vector.
+ * @param goal is an instance of a message of type move_base_msgs::MoveBaseGoal
+ * @return void None.
+ */
+void Set_Current_Orientation(std::vector<double>& Next_Goal,
+                        move_base_msgs::MoveBaseGoal & goal);
+};
+
+#endif  // CATKIN_WS_SRC_VACUUM_BOT_INCLUDE_GOALS_H_
