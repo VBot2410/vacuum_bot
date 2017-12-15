28f2c133d9321e10cc86376b072fe792d75c1568
VBot2410 <vaibhav24.vb@gmail.com>
2017-12-14 21:10:51 -0500

    Task_ID:Doc_03_01,Dev_03_01, Updated Documentation to reflect code changes. Made Goal points of type const.

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['clean_5froom',['Clean_Room',['../classCleaner.html#a6a742590ac6256a15fcfaf6853fd6a97',1,'Cleaner']]],
-  ['cleaner',['Cleaner',['../classCleaner.html#abca01c3dfb02f02c20a3ec278bb874ba',1,'Cleaner']]]
+  ['cleaner',['Cleaner',['../classCleaner.html#a4e08e96dad7293b243e7a6856929e21b',1,'Cleaner']]]
 ];
